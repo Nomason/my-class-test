0ac96049f6610aff3a7e6b28d45fd026ddb3373c
Nomason <nomasonmedia@gmail.com>
2023-10-24 13:26:51 +0100

    my classwork

---
+++ Script.js
@@ -0,0 +1,40 @@
+
+
+
+function multiplyArray (arr){
+
+    let emptyArr = []
+    let returnArra = []
+    for(i=0; i<arr.length; i++){
+        emptyArr.push(arr[i] * 2)
+    }
+    returnArra.push(emptyArr[0])
+    for(j=0; j<emptyArr.length; j++){
+        returnArra.push(emptyArr[j])
+    }
+    return returnArra
+}
+multiplyArray([1,2,3,4])
+
+
+
+
+
+
+
+
+
+    
+// return (...multiplyArr.slice [0, 1], ...multiplyArr);
+// }
+// console.log(multiplyArr) [3, 5, 8, 2]
+
+// array1.push(arr[i]*2)
+// }
+// array2.push(array1 (0))
+// for (i=0; i<array1.length; i++){
+//     array2.push(array[i]*2)
+
+// }
+
+
